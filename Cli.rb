b8d514f893b5b85b0ba976e683d7c40a4d32bd70
YurkaG <yuri.groger@gmail.com>
2014-01-06 22:03:14 +0200

    Added comments to the Cli,rb

--- Cli.rb
+++ Cli.rb
@@ -2,6 +2,13 @@
 # This is a simple client for Ser.rb
 # Created by Yuri
 #
+# This client will open connectio on port 2000 to the server
+# It read input from keyboard and send it to the server.
+# It read data back from the server
+# Client will send 0 to server for disconnect
+# To get data client will send 1 to 10
+# To get all the history the client will send 999
+#
 
 require 'socket'      # Sockets are in standard library
 
@@ -11,15 +18,15 @@ int = 1
 line = "ttttttt"
 s = TCPSocket.open(hostname, port)
 
-while line and int.to_i != 0 # Read lines from the socket
+while int.to_i != 0 
 	print "Enter num 0..10: "
-	int = gets.chomp.to_i
+	int = gets.chomp.to_i # Read number from the keyboard
 	if int >= 0 and int <= 999 
 		s.puts int
 		if int.to_i != 999
-			line = s.gets
+			line = s.gets # Read the responce from server
 		else
-			line = s.recv(1024)
+			line = s.recv(1024) #Read the history from server
 		end
 		if line
 			print "\nServer responded: " + line
