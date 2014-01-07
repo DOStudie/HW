97da294352566a197d4f7527a8c1b9648be13647
YurkaG <yuri.groger@gmail.com>
2014-01-07 15:42:38 +0200

    Client that pull history each 5s

---
+++ Cli_hacked.rb
@@ -0,0 +1,24 @@
+#
+# This is a simple client for Ser.rb
+# Created by Yuri
+#
+# This client will open connectio on port 2000 to the server and pull history each 5s.
+#
+
+require 'socket'      # Sockets are in standard library
+
+hostname = 'localhost'
+port = 2000
+kint = 999 # Keyboard input
+soc = TCPSocket.open(hostname, port) # Open session to server
+
+loop {
+	soc.write(100000 + kint.to_s.length)
+	soc.write(kint) # Send data to Server
+
+	rline = soc.recv(soc.recv(6).to_i - 100000)
+
+	print "Server responded: " + rline.chomp + "\n"
+	sleep(5)
+}
+soc.close               # Close the socket when done
